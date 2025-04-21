bf561d939772ea4c56ce3801ffffc21b36956bb6
Meghan31 <pulakhandammegha@gmail.com>
2025-04-21 02:06:11 -0600

    Update Prisma import path to use generated client and comment out old import

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -89,7 +89,8 @@
 // }
 
 // src/lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../build/generated/prisma';
 
 // Create a simple PrismaClient instance
 export const prisma = new PrismaClient({
